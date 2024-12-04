99c176c4baf160c95cad46b78029c2c443296e9d
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-12-04 01:23:57 -0300

    feat: add main application test class for project setup

---
+++ src/main/java/br/edu/ifpe/manager/ResourcesmanagementApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcesmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcesmanagementApplication.class, args);
+	}
+
+}
